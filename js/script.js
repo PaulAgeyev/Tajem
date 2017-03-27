6103e7708eed3e2bccaa736f7f8762161e1b20dc
Pavel <pavel@Pavels-MacBook-Pro.local>
2017-03-27 18:32:44 +0300

    no message

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: 3000
+    });
+});
