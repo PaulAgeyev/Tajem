44641b8f9133bdaea4337b43d762fa6317b5f898
Pavel <pavel@Pavels-MacBook-Pro.local>
2017-04-02 16:54:57 +0300

    no message

--- js/script.js
+++ js/script.js
@@ -3,8 +3,11 @@ var swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
         slidesPerView: 1,
         paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
         spaceBetween: 30,
         loop: true,
         autoplay: 3000
-    });
 });
+});
+    
